98698b0bd23d3c8a7b3e9abfe88b1b71d1fb35e6
AnishSapre <anish.sapre654@gmail.com>
2025-05-12 22:43:40 +0530

    Initial Commit

---
+++ 231A.py
@@ -0,0 +1,9 @@
+number = input()
+problems = 0
+
+for i in range(int(number)):
+    petya,vasya,tonya = input().split(" ")
+    if int(petya) + int(vasya) + int(tonya) >= 2:
+        problems += 1
+
+print(problems)
